39ea731153e47653392bc6cd31cf22e34801b273
Ali <Zhanabekov.ali@gmail.com>
2023-01-20 09:52:49 +0600

    HomeWork05

---
+++ script.js
@@ -0,0 +1,15 @@
+const ageTabe = document.getElementById('age-table');
+
+
+const newLabel = ageTabe.querySelectorAll('label');
+
+
+const td = ageTabe.querySelector('td');
+
+const body = document.querySelector('body');
+
+const search = body.firstElementChild;
+
+const firstInput = search.querySelector('input');
+
+const lastElement = forms.lastElementChild;
